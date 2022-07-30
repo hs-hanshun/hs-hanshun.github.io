4c3d1b1f1c262f3754e7c4fe25e8a23ffc992530
hassan <hassanbox@qq.com>
2022-07-30 21:17:03 +0800

    init

---
+++ .vitepress/config.js
@@ -0,0 +1,40 @@
+const { getPosts, generatePaginationPages } = require('./theme/serverUtils')
+
+async function config() {
+    const pageSize = 2
+    await generatePaginationPages(pageSize)
+    return {
+        title: 'hassan',
+        base:'/',
+        description: 'vitepress,blog,blog-theme',
+        themeConfig: {
+            posts: await getPosts(),
+            pageSize: pageSize,
+            website: 'https://github.com/hs-hanshun/hs-hanshun.git', //copyright link
+            // 评论的仓库地址
+            comment: {
+                repo: 'hs-hanshun',
+                themes: 'github-light',
+                issueTerm: 'pathname'
+            },
+            nav: [
+                { text: 'Home', link: '/' },
+                { text: 'Archives', link: '/pages/archives' },
+                { text: 'Tags', link: '/pages/tags' },
+                { text: 'About', link: '/pages/about' }
+                // { text: 'Airene', link: 'http://airene.net' }  -- External link test
+            ]
+        },
+        srcExclude: ['README.md'] // exclude the README.md , needn't to compiler
+        /*
+        vite: {
+            build: { minify: false }
+        },
+        optimizeDeps: {
+            keepNames: true
+        }
+        */
+    }
+}
+
+module.exports = config()
---
+++ posts/tech/welcome.md
@@ -0,0 +1,8 @@
+---
+date: 2022-07-30
+title: 零拷贝技术
+tags:
+- Java
+description: Java 技术
+---
+这是个好的开发端
